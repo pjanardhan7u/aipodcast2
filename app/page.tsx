a42699fcc57ad168c3b0fc1b3a5f5879b3520903
P Janardhan <pjanardhan7u@gmail.com>
2024-08-19 10:41:56 +0530

    intial commet project setup is completed

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+
+
+const page = () => {
+  return (
+    <div className='mt-8 flex flex-col gap-9'>
+      <section className='flex flex-col gap-5'>
+        <h1 className='text-20 font-bold'>Trending Podcasts</h1>
+        <Button variant="outline">Button</Button>
+      </section>
+      
+
+      </div>
+  )
+}
+
+export default page
