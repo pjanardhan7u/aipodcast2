9d519130347f0d719af82f953ec45be2f2d05a65
P Janardhan <pjanardhan7u@gmail.com>
2024-08-21 01:10:39 +0530

    need to db queries are defined need to connect with clerk

---
+++ convex/schema.ts
@@ -0,0 +1,33 @@
+import { defineSchema, defineTable } from "convex/server";
+import {v} from "convex/values";
+
+//   storage refers to a feature within the Convex platform, which is used to such as audio files, images, or other binary data.
+export default defineSchema({
+    podcasts:defineTable({
+        user:v.id('users'),
+        podcastTitle:v.string(),
+        podcastDescription:v.string(),
+        audioUrl:v.string(),
+        audioSorageId: v.optional(v.id('_storage')),
+        imageUrl:v.string(),
+        imageStorageId:v.optional(v.id('_storage')),
+        author:v.string(),
+        authorId:v.string(),
+        authorImageUrl:v.string(),
+        voicePrompt:v.string(),
+        ImagePrompt:v.string(),
+        voiceType:v.string(),
+        audioDuration:v.number(),
+        views:v.number(),
+    })
+    .searchIndex('search_author', {searchField: 'author'})
+    .searchIndex('search_title', {searchField: 'podcastTitle'})
+    .searchIndex('search_body', {searchField: 'podcastDescription'}),
+    users:defineTable({
+        name:v.string(),
+        email:v.string(),
+        imageUrl:v.string(),
+        clearkId:v.string(),
+    }),
+});
+
---
+++ convex/users.ts
@@ -0,0 +1,106 @@
+import {v,ConvexError} from "convex/values";
+import {internalMutation,query} from "./_generated/server";
+
+export const getUserById=query({
+    args:{clerkId:v.string()},
+    handler:async (ctx,args)=>{
+        const user=await ctx.db.query("users")
+                        .filter((q)=>q.eq(q.field("clerkId"),args.clerkId))
+                        .unique();
+
+        if(!user){
+            throw new ConvexError("User not found");
+        } 
+        return user;
+    },
+})
+
+export const getTopUserByPodcastCount = query({
+    args:{},
+    handler: async(ctx,args)=>{
+        const user=await ctx.db.query("users").collect();
+
+        const userData = await Promise.all(
+            user.map(async (u)=>{
+                const podcasts = await ctx.db.query("podcasts")
+                                .filter((q)=>q.eq(q.field("authorId"),u.clerkId))
+                                .collect();
+                const sortedPodcasts=podcasts.sort((a,b)=>b.views-a.views);
+                return {
+                    ...u,
+                    totalPodcasts:podcasts.length,
+                    podcast:sortedPodcasts.map((p)=>({
+                        title:p.title,
+                        views:p.views,
+                    })),
+                };
+            })
+        )
+        return userData.sort((a,b)=>b.totalPodcasts-a.totalPodcasts);
+    },
+})
+
+
+export const createUser = internalMutation({
+    args:{
+        name:v.string(),
+        email:v.string(),
+        imageUrl:v.string(),
+        clerkId:v.string(),
+    },handler:async(ctx,args)=>{
+            await ctx.db.insert("users",{
+            name:args.name,
+            email:args.email,
+            imageUrl:args.imageUrl,
+            clerkId:args.clerkId,
+        });
+
+        },
+})
+
+export const updateUser = internalMutation({
+    args:{
+        clerkId:v.string(),
+        email:v.optional(v.string()),
+        imageUrl:v.optional(v.string()),
+    },handler:async(ctx,args)=>{
+        const user=await ctx.db.query("users")
+                 .filter((q)=>q.eq(q.field("clerkId"),args.clerkId))
+                    .unique();
+        if(!user){
+            throw new ConvexError("User not found");
+        }
+
+        await ctx.db.patch(user._id,{
+            email:args.email,
+            imageUrl:args.imageUrl,
+        });
+
+        const podcast=await ctx.db.query("podcasts")
+            .filter((q)=>q.eq(q.field("authorId"),args.clerkId))
+            .collect();
+
+        await Promise.all(
+            podcast.map(async (p)=>{
+                await ctx.db.patch(p._id,{
+                    authorImageUrl:args.imageUrl,
+                });
+            })
+        )
+    },
+})
+
+export const deleteUser = internalMutation({
+    args:{clerkId:v.string()},
+    handler: async(ctx,args)=>{
+        const user=await ctx.db.query("users")
+            .filter((q)=>q.eq(q.field("clerkId"),args.clerkId))
+            .unique();
+        
+        if(!user){
+            throw new ConvexError("User not found");
+        }
+
+        await ctx.db.delete(user._id);
+    },
+})
