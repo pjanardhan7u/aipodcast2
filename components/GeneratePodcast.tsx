8e611a0b00a5b5bd8793f439c0a8d55783c5b413
P Janardhan <pjanardhan7u@gmail.com>
2024-08-22 14:23:16 +0530

    need to integrate openai

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import { GeneratePodcastProps } from '@/types'
+import React, { useState } from 'react'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
+import { Button } from './ui/button'
+import { Loader } from 'lucide-react'
+import { useAction, useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
 
-const GeneratePodcast = () => {
+
+const useGeneratePodcast =   ({setAudio, voiceType, voicePrompt, setAudioStorageId
+}: GeneratePodcastProps) => {
+    const [isGenerating, setIsGenerating] = useState(false);
+
+    const generatePodcast = async () => {
+        setIsGenerating(true);
+        setAudio('');
+
+        if(!voicePrompt){
+            return setIsGenerating(false);
+        }
+
+        try{
+            // const response = await getPodcastAudio({
+            //     voice: voiceType,
+            //     input: voicePrompt
+            //   })
+
+        }
+        catch(error){
+            console.log('Error generating podcast', error)
+            setIsGenerating(false);
+        }
+
+    }
+
+    return {
+        isGenerating,
+        generatePodcast:()=>{}
+    }
+}
+
+
+
+
+
+
+
+const GeneratePodcast = (props:GeneratePodcastProps) => {
+    
+    const {isGenerating , GeneratePodcast} = useGeneratePodcast(props)
+
+
   return (
     <div>
-        
+    <div className="flex flex-col gap-2.5">
+      <Label className="text-16 font-bold text-white-1">
+        AI Prompt to generate Podcast
+      </Label>
+      </div>
+      <Textarea 
+          className="input-class font-light focus-visible:ring-offset-orange-1"
+          placeholder='Provide text to generate audio'
+          rows={5}
+          value={props.voicePrompt}
+          onChange={(e) => props.setVoicePrompt(e.target.value)}
+        />
+        <div className="mt-5 w-full max-w-[200px]">
+            <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1" >
+                {isGenerating ? (
+                <>
+                    Generating
+                    <Loader size={20} className="animate-spin ml-2" />
+                </>
+                ) : (
+                'Generate'
+                )}
+            
+            </Button>
+        </div>
+
+        {props.audio && (
+        <audio 
+          controls
+          src={props.audio}
+          autoPlay
+          className="mt-5"
+          onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)}
+        />
+      )}
+
     </div>
   )
 }
