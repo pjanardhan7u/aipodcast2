cadd401528b0ca08b2186973295e4d0983c9f57c
P Janardhan <pjanardhan7u@gmail.com>
2024-08-22 20:01:04 +0530

    still need to solve bugs

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+
 import { voiceDetails } from "@/constants";
 import { Button } from "@/components/ui/button"
 import {
